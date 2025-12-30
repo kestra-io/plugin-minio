c1297aecb01d1c9ae90b7b7b312998967522d060
ben8t <pimpaudben@gmail.com>
2025-12-30 15:39:52 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/minio/package-info.java
+++ src/main/java/io/kestra/plugin/minio/package-info.java
@@ -1,4 +1,6 @@
-@PluginSubGroup(title = "MinIO", categories = PluginSubGroup.PluginCategory.STORAGE)
+@PluginSubGroup(title = "MinIO", categories = PluginSubGroup.PluginCategory.STORAGE,
+    categories = { PluginSubGroup.PluginCategory.DATA, PluginSubGroup.PluginCategory.INFRASTRUCTURE }
+)
 package io.kestra.plugin.minio;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
