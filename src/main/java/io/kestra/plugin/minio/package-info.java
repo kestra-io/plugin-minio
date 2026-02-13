5a1a3b9a39b299f5889030ed205c32e36b3da0a2
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:12:31 +0100

    chore: update plugin categories (#91)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    * chore: merge package categories attribute
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/minio/package-info.java
+++ src/main/java/io/kestra/plugin/minio/package-info.java
@@ -1,4 +1,5 @@
-@PluginSubGroup(title = "MinIO", categories = PluginSubGroup.PluginCategory.STORAGE)
+@PluginSubGroup(title = "MinIO",categories = { PluginSubGroup.PluginCategory.DATA, PluginSubGroup.PluginCategory.INFRASTRUCTURE }
+)
 package io.kestra.plugin.minio;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
